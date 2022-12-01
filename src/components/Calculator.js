28b26bd15800050c8eaf59ca93187a8de99d12ee
Emmanuel <emmanuellmhango@gmail.com>
2022-12-01 06:17:44 +0400

    Adding calculator logic. Added 2 files, calculate.js and operate.js in the logic directory

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,35 +1,148 @@
 import React from 'react';
+import Calculate from '../logic/calculate';
 
 class MyCalculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    this.PutNumber = this.PutNumber.bind(this);
+  }
+
+  PutNumber = (e) => {
+    const operations = ['+', '-', 'x', '%', '÷'];
+    const newVal = e.target.value;
+    const stateVal = this.state;
+    const betterVal = stateVal.next === null ? 0 : stateVal.next;
+    if (operations.includes(newVal)) {
+      const result = Calculate(stateVal, newVal);
+      if (result.total !== null && result.next === null) {
+        this.setState(
+          {
+            total: result.total,
+            next: null,
+            operation: newVal,
+          },
+        );
+      } else {
+        this.setState(
+          {
+            total: betterVal,
+            next: null,
+            operation: newVal,
+          },
+        );
+      }
+    } else if (newVal === '=') {
+      if (stateVal.next === null) {
+        if (stateVal.total !== null) {
+          this.setState(
+            {
+              total: stateVal.total,
+              next: null,
+              operation: null,
+            },
+          );
+        } else {
+          this.setState(
+            {
+              total: null,
+              next: null,
+              operation: null,
+            },
+          );
+        }
+      } else if (stateVal.total === null && stateVal.next !== null) {
+        this.setState(
+          {
+            total: null,
+            next: stateVal.next,
+            operation: null,
+          },
+        );
+      } else {
+        const result = Calculate(stateVal, newVal);
+        this.setState(
+          {
+            total: result.total,
+            next: null,
+            operation: null,
+          },
+        );
+      }
+    } else if (newVal === 'AC') {
+      this.setState(
+        {
+          total: null,
+          next: null,
+          operation: null,
+        },
+      );
+    } else if (newVal === '+/-') {
+      const result = Calculate(stateVal, newVal);
+      if (result.total !== null && result.total < 1) {
+        this.setState(
+          {
+            total: result.total,
+          },
+        );
+      }
+      if (result.next !== null && result.next < 1) {
+        this.setState(
+          {
+            next: result.next,
+          },
+        );
+      }
+    } else {
+      const newStateVal = parseInt(betterVal === 0 ? newVal : `${betterVal}${newVal}`, 10);
+      this.setState(
+        {
+          next: newStateVal,
+        },
+      );
+    }
   }
 
   render() {
+    const { total, next, operation } = this.state;
+    let answer = null;
+    if (total === null && next !== null) {
+      answer = next;
+    } else if (total !== null && next === null) {
+      answer = total;
+    } else if (total !== null && next !== null) {
+      answer = next;
+    } else {
+      answer = 0;
+    }
+
     return (
       <div className="calculator-container">
-        <div className="calculator-div">
-          <div className="display-screen">0</div>
-          <div className="item-ac item-items">AC</div>
-          <div className="item-plus-minus item-items">+/-</div>
-          <div className="item-modulus item-items">%</div>
-          <div className="item-multiply item-controls">/</div>
-          <div className="item-7 item-items">7</div>
-          <div className="item-8 item-items">8</div>
-          <div className="item-9 item-items">9</div>
-          <div className="item-multiply item-controls">x</div>
-          <div className="item-4 item-items">4</div>
-          <div className="item-5 item-items">5</div>
-          <div className="item-6 item-items">6</div>
-          <div className="item-minus item-controls">-</div>
-          <div className="item-1 item-items">1</div>
-          <div className="item-2 item-items">2</div>
-          <div className="item-3 item-items">3</div>
-          <div className="item-plus item-controls">+</div>
-          <div className="item-0 zero item-items">0</div>
-          <div className="item-dot item-items">.</div>
-          <div className="item-equals item-controls">=</div>
+        <div className="calculator-div" id={operation}>
+          <div className="display-screen" id="display">{ answer }</div>
+          <button type="button" className="item-ac item-items" value="AC" onClick={this.PutNumber}>AC</button>
+          <button type="button" className="item-plus-minus item-items" value="+/-" onClick={this.PutNumber}>+/-</button>
+          <button type="button" className="item-modulus item-items" value="%" onClick={this.PutNumber}>%</button>
+          <button type="button" className="item-multiply item-controls" value="÷" onClick={this.PutNumber}>÷</button>
+          <button type="button" className="item-7 item-items" value="7" onClick={this.PutNumber}>7</button>
+          <button type="button" className="item-8 item-items" value="8" onClick={this.PutNumber}>8</button>
+          <button type="button" className="item-9 item-items" value="9" onClick={this.PutNumber}>9</button>
+          <button type="button" className="item-multiply item-controls" value="x" onClick={this.PutNumber}>x</button>
+          <button type="button" className="item-4 item-items" value="4" onClick={this.PutNumber}>4</button>
+          <button type="button" className="item-5 item-items" value="5" onClick={this.PutNumber}>5</button>
+          <button type="button" className="item-6 item-items" value="6" onClick={this.PutNumber}>6</button>
+          <button type="button" className="item-minus item-controls" value="-" onClick={this.PutNumber}>-</button>
+          <button type="button" className="item-1 item-items" value="1" onClick={this.PutNumber}>1</button>
+          <button type="button" className="item-2 item-items" value="2" onClick={this.PutNumber}>2</button>
+          <button type="button" className="item-3 item-items" value="3" onClick={this.PutNumber}>3</button>
+          <button type="button" className="item-plus item-controls" value="+" onClick={this.PutNumber}>+</button>
+          <button type="button" className="item-0 zero item-items" value="0" onClick={this.PutNumber}>0</button>
+          <button type="button" className="item-dot item-items" value="." onClick={this.PutNumber}>.</button>
+          <button type="button" className="item-equals item-controls" value="=" onClick={this.PutNumber}>=</button>
         </div>
       </div>
     );
