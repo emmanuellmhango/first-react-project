54bb9fa29fe8965c48214adefaf89f06a12b353d
Emmanuel <emmanuellmhango@gmail.com>
2022-12-02 04:15:04 +0400

    Changed the code to use hooks. Changed files are App.js and componets/Calculator.js

--- src/App.js
+++ src/App.js
@@ -2,21 +2,12 @@ import './App.css';
 import React from 'react';
 import MyCalculator from './components/Calculator';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <MyCalculator />
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <MyCalculator />
+    </header>
+  </div>
+);
 
 export default App;
