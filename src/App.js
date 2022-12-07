9e6842cc7f05624b9fee2106ffba9255157eef15
Emmanuel <emmanuellmhango@gmail.com>
2022-12-07 04:47:44 +0400

    Add Routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import './App.css';
 import React from 'react';
-import MyCalculator from './components/Calculator';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import CalcHandler from './components/CalcHandler';
+import Quote from './components/Quote';
+import NavBar from './components/NavBar';
 
 const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <MyCalculator />
-    </header>
-  </div>
+  <>
+    <NavBar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/calculator" element={<CalcHandler />} />
+      <Route path="/quote" element={<Quote />} />
+    </Routes>
+  </>
 );
 
 export default App;
+
+/**
+ *
+ * <div className="App">
+    <header className="App-header">
+
+    </header>
+  </div>
+ */
