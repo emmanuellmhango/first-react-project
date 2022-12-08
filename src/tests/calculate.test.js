5f406f5007f5bf7adde05d42bf538fb9d3857029
inakiesk <inakieskruiz@gmail.com>
2022-12-08 11:40:06 -0600

    add calculate function jest test

--- src/tests/calculate.test.js
+++ src/tests/calculate.test.js
@@ -2,7 +2,7 @@ import calculate from '../logic/calculate';
 
 describe('Testing calute function', () => {
   test('Testing if button is AC ', () => {
-    // Arrange obj, buttonName
+    // Arrange
     const obj = {
       total: '20',
       next: '10',
@@ -15,7 +15,7 @@ describe('Testing calute function', () => {
     expect(result).toEqual({ total: null, next: null, operation: null });
   });
   test('Testing if input is number 0', () => {
-    // Arrange obj, buttonName
+    // Arrange
     const obj = {
       total: null,
       next: '0',
@@ -28,16 +28,16 @@ describe('Testing calute function', () => {
     expect(result).toEqual({});
   });
   test('Testing if input is number betwen 1 - 9. in our case: 5', () => {
-    // Arrange obj, buttonName
+    // Arrange
     const obj = {
-      total: null,
-      next: '10',
+      total: 30,
+      next: null,
       operation: '+',
     };
     const btnName = '5';
     // Act
     const result = calculate(obj, btnName);
     // Assert
-    expect(result).toEqual({ total: '15', next: null, operation: null }, '5');
+    expect(result).toEqual({ total: 30, next: '5', operation: '+' }, '5');
   });
 });
